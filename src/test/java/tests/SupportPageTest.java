48cf00ed7b08af120b7aed021eb26dfe77196c43
tulshidast <tulsidast77@gmail.com>
2026-08-07 07:54:15 +0530

    added test cases on support

--- src/test/java/tests/SupportPageTest.java
+++ src/test/java/tests/SupportPageTest.java
@@ -1,5 +1,10 @@
 package tests;
 
-public class SupportPageTest {
+public class SupportPageTest extends BaseTest {
+
+	// Add test methods specific to the Support page here
+	public void testSupportPageNavigation() {
+		// Implement test logic for Support page navigation
+	}
 
 }
