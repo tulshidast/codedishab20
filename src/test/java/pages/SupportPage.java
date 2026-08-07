f2d8597cb6c37e3f2f439d3d74a0415c24f393ce
tulshidast <tulsidast77@gmail.com>
2026-08-07 07:36:59 +0530

    Added support page and related test case

---
+++ src/test/java/pages/SupportPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SupportPage extends BasePage {
+
+	public SupportPage(WebDriver driver) {
+		super(driver);
+	}
+
+	// Add methods specific to the Support page here
+	@FindBy(css = "img[src='images/home.gif']")
+	WebElement backToHomeButton;
+
+	public void clickBackToHome() {
+		backToHomeButton.click();
+	}
+
+}
